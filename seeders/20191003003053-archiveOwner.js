44bfc35aa2056be5fdc629667f8bf4698a744227
samuel-2018 <45561451+samuel-2018@users.noreply.github.com>
2019-10-03 20:05:18 -0400

    seeding for user 1, (for use as an Archive)

---
+++ seeders/20191003003053-archiveOwner.js
@@ -0,0 +1,48 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+
+    return queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          userId: 1,
+          firstName: "The",
+          lastName: "Archive",
+          username: "Archive",
+          country: "Database",
+          // Caution: Password will not be hashed.
+          password: "pass",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+    return queryInterface.bulkDelete("users", null, {});
+  }
+};
+
+// This file can be run at the terminal:
+// $ npx sequelize db:seed:all
